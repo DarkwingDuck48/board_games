926375cf191652c0193d5593e8945dcd41103c01
Maxim Britvin <maksbritvin@gmail.com>
2023-09-08 17:49:02 +0300

    test action

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def hello():
+    return {"message": "hello World!"}
+
+
+Very_big_str = '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111'
